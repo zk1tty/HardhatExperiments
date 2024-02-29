74abebee1ce2bc73747fc9b2f632392e49602053
godeva <godeva@wpi.edu>
2024-02-29 16:05:18 -0700

    experiment code

---
+++ packages/nextjs/next.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/debug',
+        permanent: false,
+      },
+    ]
+  },
+};
+
+module.exports = nextConfig;
---
+++ packages/nextjs/scaffold.config.ts
@@ -0,0 +1,58 @@
+import * as chains from "viem/chains";
+
+export type ScaffoldConfig = {
+  targetNetworks: readonly chains.Chain[];
+  pollingInterval: number;
+  alchemyApiKey: string;
+  walletConnectProjectId: string;
+  onlyLocalBurnerWallet: boolean;
+  walletAutoConnect: boolean;
+};
+
+const scaffoldConfig = {
+  // The networks on which your DApp is live
+  targetNetworks: [chains[process.env.NEXT_PUBLIC_DEFAULT_NETWORK || "hardhat"]],
+
+  // The interval at which your front-end polls the RPC servers for new data
+  // it has no effect if you only target the local network (default is 4000)
+  pollingInterval: 30000,
+
+  // This is ours Alchemy's default API key.
+  // You can get your own at https://dashboard.alchemyapi.io
+  // It's recommended to store it in an env variable:
+  // .env.local for local testing, and in the Vercel/system env config for live apps.
+  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
+
+  // This is ours WalletConnect's default project ID.
+  // You can get your own at https://cloud.walletconnect.com
+  // It's recommended to store it in an env variable:
+  // .env.local for local testing, and in the Vercel/system env config for live apps.
+  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
+
+  // Only show the Burner Wallet when running on hardhat network
+  onlyLocalBurnerWallet: true,
+
+  /**
+   * Auto connect:
+   * 1. If the user was connected into a wallet before, on page reload reconnect automatically
+   * 2. If user is not connected to any wallet:  On reload, connect to burner wallet if burnerWallet.enabled is true && burnerWallet.onlyLocal is false
+   */
+  walletAutoConnect: true,
+} as const satisfies ScaffoldConfig;
+
+Object.assign(scaffoldConfig.targetNetworks[0].rpcUrls, {
+    "default": {
+            "http": [
+                    process.env.NEXT_PUBLIC_RPC_URL
+            ]
+        },
+        "public": {
+            "http": [
+                process.env.NEXT_PUBLIC_RPC_URL
+            ]
+        }
+});
+
+console.log(JSON.stringify(scaffoldConfig.targetNetworks[0], null, 4))
+
+export default scaffoldConfig;
